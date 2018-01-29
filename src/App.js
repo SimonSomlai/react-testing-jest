20b978e661a593186f6e991dd0df175b7e61e8a6
Simon Somlai <info@truetech.be>
2018-01-29 21:56:03 +0100

    change

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     this.state = {
       notes: [
         {
-          id: 18,
+          id: 19,
           title: "First Tas",
           description: "Pick Up Paycheck"
         }, {
